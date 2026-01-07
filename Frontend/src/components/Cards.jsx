6d6b9e55eab8fb0176ba271bd9a21ef2f8583e33
Manojyadav72 <manojyadav9027@gmail.com>
2026-01-07 14:50:53 +0530

    Frontend done

---
+++ Frontend/src/components/Cards.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+function Cards({item}) {
+  return (
+    <>
+      <div className="mt-6 my-3 p-3">
+        <div className="card bg-base-100 w-92 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
+          <figure>
+            <img src={item.image} alt="book"/>
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              {item.name}
+              <div className="badge badge-secondary">{item.category}</div>
+            </h2>
+            <p>{item.title}</p>
+            <div className="card-actions justify-between">
+              <div className="badge border rounded-xl badge-outline">${item.price}</div>
+              <div className="cursor-pointer px-2 py-1 border-[2px] rounded-full hover:bg-pink-400 hover:text-white duration-300">Buy now</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cards;
---
+++ Frontend/src/components/Signup.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import Login from "./Login";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+
+function Signup() {
+     const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm()
+  
+    const onSubmit = (data) => console.log(data)
+  
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <div className="w-[500px] border-[2px] shadow-md p-5 rounded-md">
+          <div className="">
+            <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <Link
+                  to="/"
+                  className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 dark:bg-slate-900 dark:text-white"
+                >
+                  ✕
+                </Link>
+              <h3 className="font-bold text-lg">SignUp</h3>
+              <div className="mt-4 space-y-2">
+                <span>Name</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Enter your fullname"
+                  className="w-80 px-3 py-1 border rounded-md outline-none dark:bg-slate-900 dark:text-white"
+                  {...register("name", { required: true })}
+                />
+                <br />
+              {errors.name && <span className="text-sm text-red-700">This field is required</span>}
+              </div>
+              <div className="mt-4 space-y-2">
+                <span>Email</span>
+                <br />
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="w-80 px-3 py-1 border rounded-md outline-none dark:bg-slate-900 dark:text-white"
+                  {...register("email", { required: true })}
+                />
+                <br />
+              {errors.email && <span className="text-sm text-red-700">This field is required</span>}
+              </div>
+              <div className="mt-6 space-y-2">
+                <span>Password</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Enter your password"
+                  className="w-80 px-3 py-1 border rounded-md outline-none dark:bg-slate-900 dark:text-white"
+                  {...register("password", { required: true })}
+                />
+                <br />
+              {errors.password && <span className="text-sm text-red-700">This field is required</span>}
+              </div>
+                <div className="mt-6 space-y-2">
+                <span>Confirm Password</span>
+                <br />
+                <input
+                  type="text"
+                  placeholder="Confirm your password"
+                  className="w-80 px-3 py-1 border rounded-md outline-none dark:bg-slate-900 dark:text-white"
+                  {...register("confirmpassword", { required: true })}
+                />
+                <br />
+              {errors.confirmpassword && <span className="text-sm text-red-700">This field is required</span>}
+              </div>
+              <div className="mt-6 flex justify-between">
+                <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-300">
+                  Signup
+                </button>
+                <p className="text-xl">
+                  Have account?{" "}                
+                   <button  className="underline text-blue-500 cursor-pointer"
+                    onClick={() =>
+                      document.getElementById("my_modal_3").showModal()
+                    }>Login</button> {" "}
+                  <Login />
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Signup;
